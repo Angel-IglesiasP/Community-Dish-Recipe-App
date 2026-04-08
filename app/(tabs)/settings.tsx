85b47e51b2f2cb9dac204d9fd971b81c2dab0e53
Aiden <aiden.larsen@edu.sait.ca>
2026-04-07 19:10:26 -0600

    settings being finicky

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,136 +1,121 @@
 import { colors } from "@/constants/colors";
+import { useState } from "react";
 import {
   Image,
   Pressable,
   ScrollView,
   StyleSheet,
+  Switch,
   Text,
   View,
 } from "react-native";
-
-type SettingValues = {
-  AutoSave: boolean;
-  RecipeDownload: boolean;
-  TextSize: number;
-  TextToSpeech: boolean;
-  ColourBlind: number;
-};
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const [autoSave, setAutoSave] = useState<false | true>(false);
+  const [recipeDownload, setRecipeDownload] = useState<true | false>(true);
+  const [textSize, setTextSize] = useState<1 | 2 | 3>(2);
+  const [textToSpeech, setTextToSpeech] = useState<false | true>(false);
+  const [colourBlind, setColourBlind] = useState<0 | 1 | 2 | 3>(0);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "flex-start",
-          top: 50,
-          gap: 30,
-        }}
-      >
+    <SafeAreaView style={{ backgroundColor: colors.secondary }}>
+      <View style={styles.header}>
         <Image
           source={require("@/assets/images/logo.png")}
-          style={{ width: 50, height: 50, borderRadius: 25 }}
-          resizeMode="contain"
+          style={styles.logo}
         />
-        <View
-          style={{
-            backgroundColor: colors.primary,
-            color: "black",
-            padding: 10,
-            borderRadius: 30,
-            alignItems: "center",
-            minWidth: 300,
-            maxHeight: 50,
-          }}
-        >
-          <Text
-            style={{ fontSize: 24, fontWeight: "bold", color: colors.surface }}
-          >
-            Settings
-          </Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.headText}>Settings</Text>
         </View>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 10,
-          }}
-        >
-          <Image
-            source={require("@/assets/images/profile.jpg")}
-            style={{ width: 300, height: 300, borderRadius: 150 }}
-          />
-          <View
-            style={{
-              backgroundColor: colors.primary,
-              padding: 15,
-              borderRadius: 32,
-            }}
-          >
-            <Text
-              style={{
-                color: colors.surface,
-                fontSize: 24,
-                fontWeight: "bold",
-              }}
-            >
-              Edit Profile
-            </Text>
+      <View style={styles.body}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.bodyInner}>
+            <Image
+              source={require("@/assets/images/profile.jpg")}
+              style={styles.profile}
+            />
+            <Pressable style={styles.buttonContainer}>
+              <Text style={styles.buttonText}>Edit Profile</Text>
+            </Pressable>
           </View>
 
-          <View style={{ flex: 1, flexDirection: "row", gap: 200 }}>
-            <View style={{ gap: 10 }}>
-              <Text style={styles.options}>Auto Save</Text>
-
-              <Text style={styles.options}>Downlaod</Text>
-
-              <Text style={styles.options}>Text Size</Text>
-
-              <Text style={styles.options}>Text to Speech</Text>
-
-              <Text style={styles.options}>Colour Blind</Text>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <View style={styles.selectContainer}>
+              <Text>Auto Save Recipe</Text>
+              <Text>Recipe Download</Text>
+              <Text>Text Size</Text>
+              <Text>Text To Speech</Text>
+              <Text>Colour Blind</Text>
             </View>
 
-            <View>
-              <Pressable>
-                <Text>Button</Text>
-              </Pressable>
-
-              <Pressable>
-                <Text>Button</Text>
-              </Pressable>
-
-              <Pressable>
-                <Text>Button</Text>
-              </Pressable>
-
-              <Pressable>
-                <Text>Button</Text>
-              </Pressable>
-
-              <Pressable>
-                <Text>Button</Text>
-              </Pressable>
+            <View style={styles.selectContainer}>
+              <Switch
+                value={autoSave}
+                onValueChange={setAutoSave}
+                trackColor={{ true: colors.primary, false: colors.secondary }}
+              />
             </View>
           </View>
-        </View>
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  options: {
-    backgroundColor: colors.primary,
-    borderRadius: 20,
-    padding: 10,
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.secondary,
+    gap: 20,
+  },
+  headText: {
+    fontSize: 32,
+    fontWeight: "bold",
     color: colors.surface,
-    fontSize: 16,
+  },
+  textContainer: {
+    backgroundColor: colors.primary,
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 70,
+  },
+  logo: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  },
+
+  body: {
+    backgroundColor: colors.surface,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 20,
+    padding: 10,
+  },
+  bodyInner: { gap: 10, alignItems: "center" },
+  profile: { width: 300, height: 300, borderRadius: 150 },
+  buttonText: { fontSize: 24, fontWeight: "bold", color: colors.surface },
+  buttonContainer: {
+    backgroundColor: colors.primary,
+    alignItems: "center",
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+
+  settings: {},
+  selectContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+    padding: 10,
+    gap: 10,
   },
 });
